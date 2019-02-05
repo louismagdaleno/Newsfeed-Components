9853cdc11be0d0949ff63dd44ac8de9800973498
Louis Magdaleno <buildthatapp@gmail.com>
2019-02-05 12:45:22 -0800

    Stretch - New Post Functionality Complete

--- Post/Post.js
+++ Post/Post.js
@@ -1,15 +1,4 @@
-class ArticleComponent {
-    constructor(obj) {
-        this.post_heading = document.createElement('h2');
-        this.post_heading.text_content = this.post_heading;
-        this.post_date = document.createElement('p');
-        this.post_date.classList.add('date');
-        this.post_date.text_content = new Date();
-        this.post_content = document.createElement('p');
-        this.post_content.text_content = this.post_content;
-        this.post_button = document.createElement('span');
-    }
-}
+
 // grab form
 let post_form = document.querySelector('.post form');
 console.log(post_form);
@@ -18,37 +7,48 @@ console.log(post_form);
 let post_form_btn = document.querySelector('form span');
 console.log(post_form_btn);
 
+
 post_form_btn.addEventListener('click', e => {
     e.target.style.backgroundColor = 'green';
     e.target.style.color = 'white';
-    let heading = post_form.firstElementChild.value;
-    console.log(heading);
+    let heading = document.querySelector('#heading').value;
+
     let content = post_form.firstElementChild.nextElementSibling.value;
-    console.log(content);
+
     if (heading.length >0 && content.length > 0) {
         let art = document.createElement('div');
         art.classList.add('article');
 
-        let content = document.createElement('p');
-        content.textContent = content;
+        let my_content = document.createElement('p');
+        my_content.textContent = content;
 
-        let heading = document.createElement('h2');
-        heading.textContent = heading;
+        let my_heading = document.createElement('h2');
+        
+        my_heading.textContent = heading;
 
         let date = document.createElement('p');
         date.classList.add('date');
         date.textContent = new Date();
 
         let post_button = document.createElement('span');
+        post_button.classList.add('expandButton');
+        post_button.textContent = 'expand';
 
 
-        art.append(heading);
-        art.append(date);
-        art.append(content);
-        art.append(post_button);
+        art.appendChild(my_heading);
+        art.appendChild(date);
+        art.appendChild(my_content);
+        art.appendChild(post_button);
   
         let parent = document.querySelector('.articles');
+
         parent.prepend(art);
+
+
+        art =  new Article(art);
+
+
+        
     }
     
     
